a09d6d8f1430c4038635eb71abfbc2133cf26e27
RODOLFO QUEIROZ <rodolfo.dqueiroz@gmail.com>
2021-06-30 19:46:36 -0300

    Crud Scafold

--- SalesWebMVC-API/Startup.cs
+++ SalesWebMVC-API/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVCAPI.Data;
 
 namespace SalesWebMVC_API
 {
@@ -33,6 +35,9 @@ namespace SalesWebMVC_API
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMVCAPIContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCAPIContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
